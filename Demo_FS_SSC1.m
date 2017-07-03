cb32e0d388522a0ebb3f56b6d97132fbe4500a3a
XuJun <nankaimathxujun@gmail.com>
2017-07-04 03:16:09 +0800

    add

--- Demo_FS_SSC1.m
+++ Demo_FS_SSC1.m
@@ -10,11 +10,11 @@ writefilepath = ['C:/Users/csjunxu/Desktop/SC/Results/' dataset '/'];
 %% Subspace segmentation methods
 % SegmentationMethod = 'LSR' ;
 % SegmentationMethod = 'LSRd0' ;
-% SegmentationMethod = 'LSR1' ;
+SegmentationMethod = 'LSR1' ;
 % SegmentationMethod = 'LSR2' ;
 
 % SegmentationMethod = 'NNLSR' ;
-SegmentationMethod = 'NNLSRd0' ;
+% SegmentationMethod = 'NNLSRd0' ;
 % SegmentationMethod = 'NPLSR' ;
 % SegmentationMethod = 'NPLSRd0' ;
 
@@ -38,7 +38,7 @@ for maxIter = [5]
     Par.maxIter = maxIter;
     for mu = [1]
         Par.mu = mu;
-        for rho = [0.5:0.1:1]
+        for rho = [1]
             Par.rho = rho;
             for lambda = [0 1]
                 Par.lambda = lambda*10^(-4);
