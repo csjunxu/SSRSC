836c55c543bb2d9f520ec0a110a491ad6e8d7ec9
XuJun <nankaimathxujun@gmail.com>
2017-07-07 15:16:22 +0800

    addd

--- Demo_MNIST1.m
+++ Demo_MNIST1.m
@@ -43,21 +43,21 @@ for nSample = [50] % number of images for each digit
 %         SegmentationMethod = 'LSR1' ;
     %     SegmentationMethod = 'LSR2' ;
     %
-%         SegmentationMethod = 'NNLSR' ;
+        SegmentationMethod = 'NNLSR' ;
     %     SegmentationMethod = 'NNLSRd0' ;
     %     SegmentationMethod = 'NPLSR' ;
     %     SegmentationMethod = 'NPLSRd0' ;
     %
 %             SegmentationMethod = 'ANNLSR' ;
-    SegmentationMethod = 'ANNLSRd0' ;
+%     SegmentationMethod = 'ANNLSRd0' ;
     %     SegmentationMethod = 'ANPLSR' ;
     %     SegmentationMethod = 'ANPLSRd0' ;
     %% Subspace segmentation
     for maxIter = [5]
         Par.maxIter = maxIter;
-        for rho = [1.22]
+        for rho = [.1:.1:2]
             Par.rho = rho;
-            for lambda = [1]
+            for lambda = [0]
                 Par.lambda = lambda*10^(-4);
                 missrate = zeros(nExperiment, 1) ;
                 for i = 1:nExperiment
