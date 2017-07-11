8c2d58704f55b6d13f26470b82ec92580948b508
XuJun <nankaimathxujun@gmail.com>
2017-07-11 22:35:24 +0800

    add

--- Demo_MNIST1.m
+++ Demo_MNIST1.m
@@ -3,7 +3,7 @@ clear;
 addpath('MNISThelpcode');
 addpath('C:\Users\csjunxu\Documents\GitHub\SubspaceCluteringCode\SSCOMP_Code\scatnet-0.2');
 %% Settings
-for nSample = [50 100] % number of images for each digit
+for nSample = [50] % number of images for each digit
     
     %% Load data
     addpath('C:\Users\csjunxu\Desktop\SC\Datasets\MNIST\')
@@ -56,7 +56,7 @@ for nSample = [50 100] % number of images for each digit
     %% Subspace segmentation
     for maxIter = [5]
         Par.maxIter = maxIter;
-        for rho = [16:1:25]
+        for rho = [15.5:1:16.5]
             Par.rho = rho;
             for lambda = [0]
                 Par.lambda = lambda*10^(-4);
