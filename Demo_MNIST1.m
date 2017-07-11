674ed631d5625b70a3e594915ab8cc409e0daa9e
XuJun <nankaimathxujun@gmail.com>
2017-07-11 12:15:34 +0800

    add

--- Demo_MNIST1.m
+++ Demo_MNIST1.m
@@ -3,7 +3,7 @@ clear;
 addpath('MNISThelpcode');
 addpath('C:\Users\csjunxu\Documents\GitHub\SubspaceCluteringCode\SSCOMP_Code\scatnet-0.2');
 %% Settings
-for nSample = [50] % number of images for each digit
+for nSample = [100 200 400 600] % number of images for each digit
     
     %% Load data
     addpath('C:\Users\csjunxu\Desktop\SC\Datasets\MNIST\')
@@ -41,7 +41,7 @@ for nSample = [50] % number of images for each digit
     %% Subspace segmentation methods
     %     SegmentationMethod = 'LSR' ;
     %     SegmentationMethod = 'LSRd0' ;
-    %         SegmentationMethod = 'LSR1' ;
+    %     SegmentationMethod = 'LSR1' ;
     %     SegmentationMethod = 'LSR2' ;
     
     %     SegmentationMethod = 'NNLSR' ;
@@ -49,14 +49,14 @@ for nSample = [50] % number of images for each digit
     %     SegmentationMethod = 'NPLSR' ;
     SegmentationMethod = 'NPLSRd0' ;
     
-    %             SegmentationMethod = 'ANNLSR' ;
-    %         SegmentationMethod = 'ANNLSRd0' ;
+    %     SegmentationMethod = 'ANNLSR' ;
+    %     SegmentationMethod = 'ANNLSRd0' ;
     %     SegmentationMethod = 'ANPLSR' ;
     %     SegmentationMethod = 'ANPLSRd0' ;
     %% Subspace segmentation
     for maxIter = [5]
         Par.maxIter = maxIter;
-        for rho = [.2:.2:5]
+        for rho = [.5:.5:5]
             Par.rho = rho;
             for lambda = [0]
                 Par.lambda = lambda*10^(-4);
