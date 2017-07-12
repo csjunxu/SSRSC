876f7d0ee995d1bfdc98f0f03ac49a59f7f71b19
XuJun <nankaimathxujun@gmail.com>
2017-07-12 18:34:52 +0800

    ad

--- Demo_MNIST2.m
+++ Demo_MNIST2.m
@@ -57,7 +57,7 @@ for nSample = [600] % number of images for each digit
     %% Subspace segmentation
     for maxIter = [5]
         Par.maxIter = maxIter;
-        for rho = [5:2:25]
+        for rho = [17.5:.5:18.5 19.5:.5:20.5]
             Par.rho = rho;
             for lambda = [0]
                 Par.lambda = lambda*10^(-4);
