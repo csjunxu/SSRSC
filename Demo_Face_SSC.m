44b94684a734006f82d4eb786e9e2dc3edab4b85
XuJun <nankaimathxujun@gmail.com>
2017-07-15 00:42:43 +0800

    add

--- Demo_Face_SSC.m
+++ Demo_Face_SSC.m
@@ -113,20 +113,13 @@ for maxIter = [5]
                     medmissrate(n) = median(missrateTot{n});
                     fprintf('Total mean error  is %.3f%%.\n ' , avgmissrate(n)) ;
                     allavgmissrate = mean(avgmissrate(avgmissrate~=0));
-                    if strcmp(SegmentationMethod, 'LSR')==1 || strcmp(SegmentationMethod, 'LSR1')==1 || strcmp(SegmentationMethod, 'LSR2')==1
+                    if strcmp(SegmentationMethod, 'LRR')==1 || strcmp(SegmentationMethod, 'LRSC')==1 || strcmp(SegmentationMethod, 'LSR')==1 || strcmp(SegmentationMethod, 'LSR1')==1 || strcmp(SegmentationMethod, 'LSR2')==1
                         matname = sprintf([writefilepath dataset '_' SegmentationMethod '_DR' num2str(DR) '_dim' num2str(dim) '_lambda' num2str(Par.lambda) '.mat']);
                         save(matname,'missrateTot','avgmissrate','medmissrate','allavgmissrate');
                     else
                         matname = sprintf([writefilepath dataset '_' SegmentationMethod '_DR' num2str(DR) '_dim' num2str(dim) '_maxIter' num2str(Par.maxIter) '_rho' num2str(Par.rho) '_lambda' num2str(Par.lambda) '.mat']);
                         save(matname,'missrateTot','avgmissrate','medmissrate','allavgmissrate');
                     end
-                end
-                if strcmp(SegmentationMethod, 'LSR')==1 || strcmp(SegmentationMethod, 'LSR1')==1 || strcmp(SegmentationMethod, 'LSR2')==1
-                    matname = sprintf([writefilepath dataset '_' SegmentationMethod '_DR' num2str(DR) '_dim' num2str(dim) '_lambda' num2str(Par.lambda) '.mat']);
-                    save(matname,'missrateTot','avgmissrate','medmissrate','allavgmissrate');
-                else
-                    matname = sprintf([writefilepath dataset '_' SegmentationMethod '_DR' num2str(DR) '_dim' num2str(dim) '_maxIter' num2str(Par.maxIter) '_rho' num2str(Par.rho) '_lambda' num2str(Par.lambda) '.mat']);
-                    save(matname,'missrateTot','avgmissrate','medmissrate','allavgmissrate');
                 end
             end
         end
