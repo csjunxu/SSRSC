a1e4b221488cb052f4f3905aacc72353013f100c
XuJun <nankaimathxujun@gmail.com>
2017-07-30 17:59:57 +0800

    add

--- Demo_Motion_Hopkins155.m
+++ Demo_Motion_Hopkins155.m
@@ -60,11 +60,11 @@ SegmentationMethod = 'DANNLSRd0';
 % SegmentationMethod = 'DANPLSR';
 % SegmentationMethod = 'DANPLSRd0';
 
-for maxIter = [5:1:10]
+for maxIter = [6:1:10]
     Par.maxIter = maxIter;
-    for s = [.75:-.05:.5]
+    for s = [.8:.05:1.2]
         Par.s = s;
-        for rho = [.025:-.001:.005]
+        for rho = [.001:.001:.02]
             Par.rho = rho;
             for lambda = [0]
                 Par.lambda = lambda*10^(-0);
