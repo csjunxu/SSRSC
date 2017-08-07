002321a1ab1e0e258d900f6a7b2bd1d42dc8f6da
XuJun <nankaimathxujun@gmail.com>
2017-08-07 08:06:39 +0800

    add

--- Demo_Face_LSR.m
+++ Demo_Face_LSR.m
@@ -41,11 +41,11 @@ SegmentationMethod = 'DANNLSR' ;
 % SegmentationMethod = 'DANNLSRd0' ;
 
 %% Subspace segmentation
-for s = .75 %[.8 .85]
+for s = [.8:.05:1]
     Par.s = s;
-    for maxIter = [4]
+    for maxIter = [5:-1:1]
         Par.maxIter = maxIter;
-        for rho = [.49:-.01:.41]
+        for rho = [1:-.1:.1]
             Par.rho = rho;
             for lambda = [0]
                 Par.lambda = lambda*10^(-0);
