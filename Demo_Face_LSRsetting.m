0c619794b34950a7147834345a5ff38eb0826f0f
XuJun <nankaimathxujun@gmail.com>
2017-08-11 12:42:45 +0800

    add

--- Demo_Face_LSRsetting.m
+++ Demo_Face_LSRsetting.m
@@ -45,7 +45,7 @@ SegmentationMethod = 'NNLSRd0' ;
 %% Subspace segmentation
 for s = [1]
     Par.s = s;
-    for maxIter = [2:1:10]
+    for maxIter = [11:1:15]
         Par.maxIter = maxIter;
         for rho = [.1:.1:1]
             Par.rho = rho;
