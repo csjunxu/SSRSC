04a19d72377acfaf4e3f44e68e0d3f0729bb2f47
XuJun <nankaimathxujun@gmail.com>
2017-08-11 21:26:42 +0800

    add

--- Demo_Face_LSRsetting.m
+++ Demo_Face_LSRsetting.m
@@ -29,8 +29,8 @@ end
 
 % SegmentationMethod = 'NNLSR' ;
 % SegmentationMethod = 'NNLSRd0' ;
-SegmentationMethod = 'NPLSR' ;
-% SegmentationMethod = 'NPLSRd0' ;
+% SegmentationMethod = 'NPLSR' ;
+SegmentationMethod = 'NPLSRd0' ;
 
 % SegmentationMethod = 'ANNLSR' ;
 % SegmentationMethod = 'ANNLSRd0' ;
@@ -45,9 +45,9 @@ SegmentationMethod = 'NPLSR' ;
 %% Subspace segmentation
 for s = [1]
     Par.s = s;
-    for maxIter = [6:1:10]
+    for maxIter = [1:1:10]
         Par.maxIter = maxIter;
-        for rho = [.1]
+        for rho = [.1:.1:.5]
             Par.rho = rho;
             for lambda = [0]
                 Par.lambda = lambda*10^(-0);
