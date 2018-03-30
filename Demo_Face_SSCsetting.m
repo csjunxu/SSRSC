2fa4793ae60411c32769ecf27ced082b9b648074
XuJun <nankaimathxujun@gmail.com>
2018-03-30 17:37:07 +0800

    add

--- Demo_Face_SSCsetting.m
+++ Demo_Face_SSCsetting.m
@@ -3,7 +3,7 @@ clear ;
 
 load 'C:\Users\csjunxu\Desktop\CVPR2018 SC\Datasets\YaleBCrop025.mat';
 dataset = 'YaleB_SSC';
-write_results_dir = ['C:/Users/csjunxu/Desktop/SC/Results/' dataset '/'];
+write_results_dir = ['C:/Users/csjunxu/Desktop/CVPR2018 SC/Results/' dataset '/'];
 if ~isdir(write_results_dir)
     mkdir(write_results_dir);
 end
@@ -18,7 +18,7 @@ end
 % SegmentationMethod = 'LSRd0' ;
 % SegmentationMethod = 'SMR' ; addpath('C:\Users\csjunxu\Desktop\SC\SMR_v1.0');
 % SegmentationMethod = 'RSIM' ; ii = 0;addpath('C:\Users\csjunxu\Desktop\SC\Ncut_9'); addpath('C:\Users\csjunxu\Desktop\SC\2015 ICCV RSIM');
-SegmentationMethod = 'SSCOMP' ; addpath('C:\Users\csjunxu\Desktop\SC\SSCOMP_Code');
+% SegmentationMethod = 'SSCOMP' ; addpath('C:\Users\csjunxu\Desktop\SC\SSCOMP_Code');
 
 % SegmentationMethod = 'NNLSR' ;
 % SegmentationMethod = 'NNLSRd0' ;
@@ -32,8 +32,8 @@ SegmentationMethod = 'SSCOMP' ; addpath('C:\Users\csjunxu\Desktop\SC\SSCOMP_Code
 
 % SegmentationMethod = 'DANNLSR';
 % SegmentationMethod = 'DANNLSRd0';
-% SegmentationMethod = 'DANPLSR';
-% SegmentationMethod = 'DANPLSRd0';
+SegmentationMethod = 'DALSR';
+% SegmentationMethod = 'DALSRd0';
 
 Repeat = 1; %number of repeations
 DR = 1; % dimension reduction
@@ -49,13 +49,13 @@ end
 alltime = [];
 jj=0;
 %% Subspace segmentation
-for maxIter = [1] % unique([floor(10*scale), ceil(10*scale)])
+for maxIter = [1:1:10] % unique([floor(10*scale), ceil(10*scale)])
     Par.maxIter = maxIter;
-    for rho = [10]
+    for rho = [1:1:10]
         Par.rho = rho;
-        for scale = [.05]
+        for scale = [.1:.1:1.1]
             Par.s = scale;
-            for lambda = [.01]
+            for lambda = [0]
                 Par.lambda = lambda;
                 for nSet = [2 3 5 8 10]
                     n = nSet;
@@ -134,10 +134,10 @@ for maxIter = [1] % unique([floor(10*scale), ceil(10*scale)])
                                         C = ANNLSR( Yfea , Par ) ;
                                     case 'ANNLSRd0'             % affine, non-negative, diagonal = 0
                                         C = ANNLSRd0( Yfea , Par ) ;
-                                    case 'ANPLSR'                 % affine, non-positive
-                                        C = ANPLSR( Yfea , Par ) ;
-                                    case 'ANPLSRd0'             % affine, non-positive, diagonal = 0
-                                        C = ANPLSRd0( Yfea , Par ) ;
+                                    case 'DALSR'                 % affine
+                                        C = DALSR( Yfea , Par ) ;
+                                    case 'DALSRd0'             % affine, diagonal = 0
+                                        C = DALSRd0( Yfea , Par ) ;
                                     case 'DANNLSR'                 % deformable, affine, non-negative
                                         C = DANNLSR( Yfea , Par ) ;
                                     case 'DANNLSRd0'             % deformable, affine, non-negative, diagonal = 0
@@ -163,7 +163,24 @@ for maxIter = [1] % unique([floor(10*scale), ceil(10*scale)])
                     medmissrate(n) = median(missrateTot{n});
                     fprintf('Total mean error  is %.3f%%.\n ' , avgmissrate(n)) ;
                     allavgmissrate = mean(avgmissrate(avgmissrate~=0));
-                    
+                      if strcmp(SegmentationMethod, 'SSC')==1 || strcmp(SegmentationMethod, 'S3C')==1
+                        matname = sprintf([write_results_dir dataset '_' SegmentationMethod '_DR' num2str(DR) '_dim' num2str(dim) '_alpha' num2str(Par.lambda) '.mat']);
+                        save(matname,'missrateTot','avgmissrate','medmissrate','allavgmissrate');
+                    elseif  strcmp(SegmentationMethod, 'LRR')==1 || strcmp(SegmentationMethod, 'LRSC')==1 || strcmp(SegmentationMethod, 'LSR')==1 || strcmp(SegmentationMethod, 'LSR1')==1 || strcmp(SegmentationMethod, 'LSR2')==1
+                        matname = sprintf([write_results_dir dataset '_' SegmentationMethod '_DR' num2str(DR) '_dim' num2str(dim) '_lambda' num2str(Par.lambda) '.mat']);
+                        save(matname,'missrateTot','avgmissrate','medmissrate','allavgmissrate');
+                    elseif strcmp(SegmentationMethod, 'NNLSR')==1 || strcmp(SegmentationMethod, 'NPLSR')==1 || strcmp(SegmentationMethod, 'NNLSRd0')==1 || strcmp(SegmentationMethod, 'NPLSRd0')==1
+                        matname = sprintf([write_results_dir dataset '_' SegmentationMethod '_DR' num2str(DR) '_dim' num2str(dim) '_maxIter' num2str(Par.maxIter) '_rho' num2str(Par.rho) '_lambda' num2str(Par.lambda) '.mat']);
+                        save(matname,'missrateTot','avgmissrate','medmissrate','allavgmissrate');
+                    elseif strcmp(SegmentationMethod, 'ANNLSR')==1 || strcmp(SegmentationMethod, 'ANNLSRd0')==1 ...
+                            || strcmp(SegmentationMethod, 'DANNLSR')==1 || strcmp(SegmentationMethod, 'DANNLSRd0')==1 ...
+                            || strcmp(SegmentationMethod, 'DALSR')==1 || strcmp(SegmentationMethod, 'DALSRd0')==1
+                        matname = sprintf([write_results_dir dataset '_' SegmentationMethod '_DR' num2str(DR) '_dim' num2str(dim) '_s' num2str(Par.s) '_maxIter' num2str(Par.maxIter) '_rho' num2str(Par.rho) '_lambda' num2str(Par.lambda) '.mat']);
+                        save(matname,'missrateTot','avgmissrate','medmissrate','allavgmissrate');
+                    elseif strcmp(SegmentationMethod, 'RSIM')==1 || strcmp(SegmentationMethod, 'S3C') == 1
+                        matname = sprintf([write_results_dir dataset '_' SegmentationMethod '_DR' num2str(DR) '_dim' num2str(dim) '.mat']);
+                        save(matname,'missrateTot','avgmissrate','medmissrate','allavgmissrate');
+                    end
                 end
             end
         end
